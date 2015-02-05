75266ad9baa306b3a379e59dc275cec05bf0523c
djfatchuck <charles.schrumpf@gmail.com>
2015-02-04 21:15:17 -0500

    Initial creation and structure

---
+++ EchoTaskReader.py
@@ -0,0 +1,8 @@
+from settings import *
+
+
+class EchoTaskReader():
+
+    def __init__(self):
+        self.user = USER
+        self.password = PASSWORD
---
+++ settings.py
@@ -0,0 +1,3 @@
+USER = ''
+PASSWORD = ''
+from local_settings import *
