1aad7d5828d06261f69f80dfccb1bba27ca4d9ed
Charles Schrumpf <charles@socialgrowthtechnologies.com>
2015-02-05 17:36:57 -0500

    Trying to add card support and removal

--- EchoReader.py
+++ EchoReader.py
@@ -7,7 +7,8 @@ if USER == '' or PASSWORD == '':
     USER = raw_input('Email: ')
     PASSWORD = getpass.getpass()
 
-class EchoTaskReader():
+
+class EchoToDoReader():
 
     def __init__(self):
         self.echo = PyEcho.PyEcho(USER, PASSWORD)
@@ -21,4 +22,24 @@ class EchoTaskReader():
                 command = task['text']
                 #TODO add commands handling
                 self.echo.deleteTask(task)
+        time.sleep(self.sleep_time)
+
+
+class EchoCardReader():
+
+    def __init__(self):
+        self.echo = PyEcho.PyEcho(USER, PASSWORD)
+        self.sleep_time = 5
+        self.command_list = {}
+
+    def run(self):
+        if self.echo:
+            cards = self.echo.cards()
+            for card in cards:
+                try:
+                    text = card['playbackAudioAction']['mainText']
+                except TypeError:
+                    pass
+                #TODO add commands handling
+                self.echo.deleteTask(card)
         time.sleep(self.sleep_time)
