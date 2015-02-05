de30ee3b98309c96e21b8409942fa2f2a2bd18dc
djfatchuck <charles.schrumpf@gmail.com>
2015-02-05 09:14:45 -0500

    quick check in to continue work later

--- EchoTaskReader.py
+++ EchoTaskReader.py
@@ -1,8 +1,23 @@
+import PyEcho
 from settings import *
 
 
 class EchoTaskReader():
 
     def __init__(self):
-        self.user = USER
-        self.password = PASSWORD
+        self.echo = PyEcho.PyEcho(USER, PASSWORD)
+        self.task_list = []
+        self.update_time = 600
+
+    def update_tasks(self):
+        tasks = self.echo.tasks()
+
+        for task in tasks:
+            if task not in self.task_list:
+                self.task_list.append(task)
+
+    def run_next_task(self):
+        task = self.task_list.pop(0)
+
+    def change_update_time(self, time):
+        self.update_time = time
